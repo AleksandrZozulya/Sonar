dbead4e34294e26d3695cea0701ec8e1f2959160
AleksandrZozulya <aleksandr.zozulya.1997@gmail.com>
2025-01-05 16:18:12 +0200

    Add additional condition

--- Sonar.cpp
+++ Sonar.cpp
@@ -1,24 +1,34 @@
 #include "Sonar.h"
 
-Sonar::Sonar(int trig, int echo){
-      this-> trig = trig;
-      this-> echo = echo;
+Sonar::Sonar(int trig, int echo)
+{
+  this->trig = trig;
+  this->echo = echo;
+}
+void Sonar::begin()
+{
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+}
+float Sonar::distance(float value)
+{
+  if (dur() > 0){
+    if (value == 0)
+    {
+      return dur() / _CM;
     }
-void Sonar::begin(){
-      pinMode(trig, OUTPUT);
-      pinMode(echo, INPUT);
+    if (value == 1)
+    {
+      return dur() / _IN;
     }
-float Sonar::distance(float value){
-      if(value == 0){
-        return dur() / _CM;
-      }
-      if(value == 1){
-        return dur() / _IN;
-      }
-    }
-int Sonar::dur(){
-      digitalWrite(trig, HIGH);
-      delayMicroseconds(10);
-      digitalWrite(trig, LOW);
-      return pulseIn(echo, true);
-    }
+  } else {
+    return 0;
+  }
+}
+int Sonar::dur()
+{
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  return pulseIn(echo, true);
+}
